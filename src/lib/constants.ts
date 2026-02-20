f7df775959df846d484c0acd0fbcab14eafaad49
יhila <hilaglazz13@gmail.com>
2026-02-20 14:29:20 +0200

    Export ADMIN_EMAILS and add SERVICE_EMAIL constant in constants file for improved accessibility and maintainability across the application.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,8 @@
 export const SERVICE_PHONE = "052-6409993";
 export const STUDIO_NAME = "עונג של פילאטיס";
-const ADMIN_EMAILS = ['hilaglazz13@gmail.com', 'oneg3gri@gmail.com'];
+export const ADMIN_EMAILS = ['hilaglazz13@gmail.com', 'oneg3gri@gmail.com'];
+export const SERVICE_EMAIL = "oneg3gri@gmail.com"
+
 
 // פונקציית עזר לייצירת לינק לוואטסאפ בקלות
 export const getWhatsAppLink = (message: string) => {
